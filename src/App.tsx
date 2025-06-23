85874bef0ee9a9e7cb4989bc27060a6d3205350d
Leonardo Malini Coradini <leomalini61@gmail.com>
2025-06-23 10:50:42 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-4">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
